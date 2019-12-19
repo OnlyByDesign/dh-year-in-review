f22b19e624ec230d92098c0ef533d8d3d79884cd
Ben Yannella <benyannella@gmail.com>
2019-12-19 10:18:25 -0500

    updated interactions file w header function

--- js/interactions.js
+++ js/interactions.js
@@ -1,1 +1,8 @@
 /* ==== DIALOGUE 2019 INTERACTION SCRIPT ==== */
+$(window).scroll(function() {
+    if ($('.header').offset().top > 100) {
+      $(this).classList.toggle('active');
+    } else {
+      $(this).classList.toggle('active');
+    };
+});
