1537195c43b7b34485c67e7c5ba0ec52b3bfc4b5
Ben Yannella <benyannella@gmail.com>
2019-12-25 23:07:15 -0500

    routine update | almost fixed parallax pinning

--- js/interactions.js
+++ js/interactions.js
@@ -1,24 +1,18 @@
-$.fn.isOnScreen = function(){ 
-    // checks if a selection is visible in viewport
-    var win = $(window);
-    var viewport = {
-      top : win.scrollTop(),
-      left : win.scrollLeft()
+$.fn.isOnScreen = function() { 
+    var win, viewport, bounds;
+    win = $(window);
+    viewport = { top : win.scrollTop(), left : win.scrollLeft() };
+    viewport.right = viewport.left + win.width(), viewport.bottom = viewport.top + win.height();
+    bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth(), bounds.bottom = bounds.top + this.outerHeight();
+    return (!( viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom ));
+  };
+  var isActive = function isActive(el, target) {
+    if (el.isOnScreen()) {
+      if (!target.parent().hasClass("active")) target.parent().addClass("active");
     };
-      viewport.right = viewport.left + win.width();
-      viewport.bottom = viewport.top + win.height();
-    var bounds = this.offset();
-      bounds.right = bounds.left + this.outerWidth();
-      bounds.bottom = bounds.top + this.outerHeight();
-    return (!(
-      viewport.right < bounds.left || 
-      viewport.left > bounds.right || 
-      viewport.bottom < bounds.top || 
-      viewport.top > bounds.bottom
-    ));
   };
   var parallaxScroll = function parallaxScroll(el, vertical, horizontal, hook, duration) {
-    // On scroll movement for html objects
     var controller = new ScrollMagic.Controller();
     var build = (function() {
       $(el[0]).each(function() {
@@ -37,40 +31,43 @@ $.fn.isOnScreen = function(){
       });
     })();
   }; 
-  var parallaxPin = function parallaxPin() {
-    // Pins objects to screen for a period of time
-    var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 300})
-      .setPin("#pin1")
-      .addIndicators({name: "1 (duration: 300)"})
-      .addTo(controller);
-  }; 
-  var isActive = function isActive(el) {
-    if (el.isOnScreen() && !el.hasClass("active")) {
-      el.addClass("active");
-    } else {
-      el.removeClass("active");
-    };
+  var parallaxPinner = function parallaxPinner(trigger, target, dur) {
+    var controller = new ScrollMagic.Controller();
+    var build = (function() {
+        var scene = new ScrollMagic.Scene({
+          triggerElement: $(trigger)[0],
+          duration: dur
+        })
+        .setPin($(target)[0])
+        .addTo(controller);
+      })();
   };
   
   (function init() {
     $(window).scroll(function() {
-      // Rewarding section
-      isActive($("#rewardingGraphTrigger"));
-    }); // On scroll functions
+      isActive($(".rewarding__bars"), $("#rewardingGraphTrigger"));
+    });
     var parallaxInit = {
       rewardInit: {
         0: parallaxScroll( $("#rewardingParallax1"), -500, 0, 1, 200 ),
         1: parallaxScroll( $("#rewardingParallax2"), -250, 0, 1, 200 )
       },
       mentalInit: {
-        0: parallaxScroll( $("#mentalParallax0"), 0, 100, 1, 200 ),
-        1: parallaxScroll( $("#mentalParallax1"), 0, -200, 1, 200 ),
-        2: parallaxScroll( $("#mentalParallax2"), 0, 400, 1, 250 ),
-        3: parallaxScroll( $("#mentalParallax3"), 0, -800, 1, 300 )
+        0: parallaxScroll( $("#mentalParallax0"), 0, 50, 1, 200 ),
+        1: parallaxScroll( $("#mentalParallax1"), 0, -100, 1, 200 ),
+        2: parallaxScroll( $("#mentalParallax2"), 0, 200, 1, 250 ),
+        3: parallaxScroll( $("#mentalParallax3"), 0, -300, 1, 300 )
+      },
+      innovationInit: {
+        0: parallaxScroll( $("#innovationBgL"), -100, 0, 1, 200 ),
+        1: parallaxScroll( $("#innovationBgR"), -400, 0, 1, 200 )
+      },
+      standInit: {
+        0: parallaxPinner( $("#standPinTrigger"), $("#standFor"), 300 )
       }
-    } // Initializer for parallax effects
+    };
     console.log('Init interaction');
-  })(); // Initializer for interaction.js
+  })(); 
     
     
     
